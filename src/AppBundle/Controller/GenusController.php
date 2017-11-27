8d2cfba413bfbc902e3fcdf683183b4bb5ff3ca2
Martin Sarsini <martin@apphancer.com>
2017-11-27 22:46:54 +0100

    Add getNotesAction in GenusController

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -9,8 +9,10 @@
 namespace AppBundle\Controller;
 
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 
@@ -19,18 +21,30 @@ class GenusController extends Controller
     /**
      * @Route("/genus/{genusName}")
      */
-    public function showAction($genusName): Response
+    public function showAction($genusName)
     {
 
+        return $this->render('genus/show.html.twig', [
+            'name' => $genusName,
+        ]);
+    }
+
+    /**
+     * @Route("/genus/{genusName}/notes")
+     * @Method("GET")
+     */
+    public function getNotesAction()
+    {
         $notes = [
-            'Octopus asked me a riddle, outsmarted me',
-            'I counted 8 legs... as they wrapped around me',
-            'Inked!',
+            ['id' => 1, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Octopus asked me a riddle, outsmarted me', 'date' => 'Dec. 10, 2015'],
+            ['id' => 2, 'username' => 'AquaWeaver', 'avatarUri' => '/images/ryan.jpeg', 'note' => 'I counted 8 legs... as they wrapped around me', 'date' => 'Dec. 1, 2015'],
+            ['id' => 3, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Inked!', 'date' => 'Aug. 20, 2015'],
         ];
 
-        return $this->render('genus/show.html.twig', [
-            'name'  => $genusName,
+        $data = [
             'notes' => $notes,
-        ]);
+        ];
+
+        return new JsonResponse($data);
     }
 }
