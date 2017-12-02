f0b72adf142af509972ac1babc62b03746c6c30d
Martin Sarsini <martin@apphancer.com>
2017-12-02 12:32:18 +0100

    Add type hint to MarkdownTransformer Service

--- src/AppBundle/Service/MarkdownTransformer.php
+++ src/AppBundle/Service/MarkdownTransformer.php
@@ -4,6 +4,7 @@
 namespace AppBundle\Service;
 
 
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use function strtoupper;
 
 class MarkdownTransformer
@@ -11,7 +12,10 @@ class MarkdownTransformer
 
     private $markdownParser;
 
-    public function __construct($markdownParser)
+    // when you need an object from inside a class, use dependency injection.
+    // And when you add the __construct() argument, type-hint it with either the class you see in debug:container or an interface if you can find one.
+    // Both totally work.
+    public function __construct(MarkdownParserInterface $markdownParser)
     {
         $this->markdownParser = $markdownParser;
     }
@@ -19,7 +23,7 @@ class MarkdownTransformer
     public function parse($str)
     {
         return $this->markdownParser
-            ->transform($str);
+            ->transformMarkdown($str);
     }
 
 }
